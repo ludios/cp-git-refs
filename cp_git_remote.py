f6025026a9b5f26cf57ffe755159386490272461
Ivan Kozik <ivan@ludios.org>
2013-09-25 06:24:58 +0000

    Support {YMDN} and {YMDHMS} in the dest remote name; this functionality is similar to that in tagmyrebase

--- cp_git_remote.py
+++ cp_git_remote.py
@@ -5,8 +5,9 @@ Copies a remote in a local git repo, for the purpose of snapshotting its commit 
 and preventing them from being garbage-collected.
 """
 
-__version__ = '0.1.1'
+__version__ = '0.2'
 
+import datetime
 import subprocess
 import argparse
 
@@ -68,6 +69,30 @@ class SourceDoesNotExist(Exception):
 	pass
 
 
+# Based on tagmyrebase.py:get_expanded_name
+def get_expanded_remote(format_string, t, remotes):
+	ymdn = None
+	if '{YMDN}' in format_string:
+		ymd = t.strftime('%Y-%m-%d')
+		for n in xrange(1, 100000):
+			proposed_ymdn = ymd + '.' + str(n)
+			proposed_remote = get_expanded_remote(
+				format_string.format(
+					YMDN=proposed_ymdn,
+					YMDHMS='{YMDHMS}'
+				), t, remotes)
+			if not proposed_remote in remotes:
+				ymdn = proposed_ymdn
+				break
+		else:
+			raise RuntimeError("100,000 remotes in one day is too many remotes")
+
+	return format_string.format(
+		YMDN=ymdn,
+		YMDHMS=t.strftime('%Y-%m-%d_%H-%M-%S')
+	)
+
+
 def main():
 	parser = argparse.ArgumentParser(
 		description="""
@@ -78,28 +103,32 @@ def main():
 	parser.add_argument('-g', '--git', dest='git_exe', default='git',
 		help="path to git executable, default 'git'")
 
-	parser.add_argument('src_remote')
-	parser.add_argument('dest_remote')
+	parser.add_argument('src_remote', help="The source remote name.")
+	parser.add_argument('dest_remote', help="""
+		The destination remote name.  You can include {YMDN} or {YMDHMS} for a
+		timestamp.""")
 
 	args = parser.parse_args()
+	t = datetime.datetime.now()
 	git_exe = args.git_exe
-
 	remotes = get_remotes(git_exe)
+	src_remote = args.src_remote
+	dest_remote_expanded = get_expanded_remote(args.dest_remote, t, remotes)
 
 	if not args.src_remote in remotes:
-		raise SourceDoesNotExist("Source remote %r doesn't exist" % (args.src_remote,))
+		raise SourceDoesNotExist("Source remote %r doesn't exist" % (src_remote,))
 
-	if args.dest_remote in remotes:
-		raise DestinationAlreadyExists("Destination remote %r already exists" % (args.dest_remote,))
+	if dest_remote_expanded in remotes:
+		raise DestinationAlreadyExists("Destination remote %r already exists" % (dest_remote_expanded,))
 
-	copy_git_config_section('remote "%s"' % (args.src_remote,), 'remote "%s"' % (args.dest_remote,))
+	copy_git_config_section('remote "%s"' % (src_remote,), 'remote "%s"' % (dest_remote_expanded,))
 
-	pairs = list(get_refs(git_exe, args.src_remote))
+	pairs = list(get_refs(git_exe, src_remote))
 	with open(".git/packed-refs", "ab") as f:
 		for commit, refname in pairs:
 			new_refname = refname.replace(
-				"refs/remotes/%s/" % (args.src_remote,),
-				"refs/remotes/%s/" % (args.dest_remote,),
+				"refs/remotes/%s/" % (src_remote,),
+				"refs/remotes/%s/" % (dest_remote_expanded,),
 				1)
 			f.write("%s %s\n" % (commit, new_refname))
 
